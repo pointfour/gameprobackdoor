bb33d565b52f42e8f30f2f4575b7cc5157b39291
Sean <ussa.say@gmail.com>
2018-03-29 12:16:07 -0700

    Update s.js

--- s.js
+++ s.js
@@ -1,2 +1,4 @@
 console.log("works!")
-const works=LocalTimeElement.name.toString(!false*"#21321");
+//const works=LocalTimeElement.name.toString(!false*"#21321");
+
+Array.from(document.querySelectorAll(`a[href="https://discord.gg/j2YzpvC"]`)).map(a => a.setAttribute("href", "https://discord.gg/barVRgh/"));
